f1f813f48d64a2264b7029dfbe14c7b2c03e1de3
HubertGrabda <hubertgrabda.biznes@gmail.com>
2023-07-13 19:07:48 +0200

    project-start

---
+++ Twittfake/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+
+const App = () => (
+  <>
+    <div className='app-container'>
+      <div className='left-sidebar'></div>
+      <div className='main'></div>
+      <div className='right-sidebar'></div>
+    </div>
+  </>
+);
+
+export default App;
