3fee0271097411f7331f2756d7235718497e6455
HubertGrabda <hubertgrabda.biznes@gmail.com>
2023-09-27 10:33:34 +0200

    rename

--- src/pages/CreateTweet.jsx
+++ src/pages/CreateTweet.jsx
@@ -1,14 +1,14 @@
 import Header from "../components/Header/Header";
 import HomeButton from "../components/HomeButton/HomeButton";
 import Navbar from "../components/Navbar/Navbar";
-import NewTweetPageContainer from "../components/NewTweetPage/NewTweetPageContainer";
+import AddTweetPageContainer from "../components/AddTweetPage/AddTweetPageContainer";
 
 const CreateNewTweet = () => (
   <>
     <Header pageTitle='Nowy tweet' />
     <HomeButton />
     <Navbar />
-    <NewTweetPageContainer />
+    <AddTweetPageContainer />
   </>
 );
 
