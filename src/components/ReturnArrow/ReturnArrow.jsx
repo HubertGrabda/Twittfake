c87477d7a5f37e567e234219975e92148eb3f8ea
HubertGrabda <hubertgrabda.biznes@gmail.com>
2023-11-07 20:08:35 +0100

    import bug

--- src/components/ReturnArrow/ReturnArrow.jsx
+++ src/components/ReturnArrow/ReturnArrow.jsx
@@ -1,12 +1,12 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./returnArrow.scss";
 import { Link, useLocation } from "react-router-dom";
 import { classNames } from "../../shared";
 import PropTypes from "prop-types";
 import { useTweetContext } from "../../hooks/useTweetContext";
 import ScrollService from "../../services/ScrollService";
 import { homePageRoute } from "../../const/routing";
+import "./ReturnArrow.scss";
 
 const ReturnArrow = ({ returnTo }) => {
   const { setFilteredTweetsData, tweets, isTagClicked, setIsTagClicked } =
